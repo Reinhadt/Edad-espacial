ed477700cfb726a5e6d9ee326f76ab1fcd255690
Omar <omavaloss@gmail.com>
2018-05-31 09:49:07 -0500

    Update Info.js

--- StarWarsAge/APP/edadmarciana/src/components/Info.js
+++ StarWarsAge/APP/edadmarciana/src/components/Info.js
@@ -4,14 +4,21 @@ const info = (props) => {
 
     const meses = ((props.datos.edad % 1)*12).toFixed(0);
 
-    return(
-        <div>
-            <h1>Tu edad en {props.datos.planeta}: {props.datos.edad}</h1>
-            <p>Meses: {props.datos.edad?meses:''}</p>
-            <p>Horas de un día en {props.datos.planeta}: {props.datos.dia}</p>
-            <p>Días de un año en {props.datos.planeta}: {props.datos.año}</p>
-        </div>
-    )
+    if(props.datos.length !== 0){
+        return(
+            <div>
+                <h1>Tu edad en {props.datos.planeta}: {props.datos.edad.toFixed(0)}</h1>
+                <p>Meses: {props.datos.edad?meses:''}</p>
+                <p>Horas de un día en {props.datos.planeta}: {props.datos.dia}</p>
+                <p>Días de un año en {props.datos.planeta}: {props.datos.año}</p>
+            </div>
+        )
+    }
+    else{
+        return(
+            <h1>Acá va tu edad</h1>
+        )
+    }
 }
 
 export default info;
